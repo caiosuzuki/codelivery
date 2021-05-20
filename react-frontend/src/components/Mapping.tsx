8939b596ec16435e70d0de51a3121ca22cf35734
caiosuzuki <cpolidoro@gmail.com>
2021-05-19 21:49:05 -0400

    styling frontend routes, allowing route to finish and including websocket

--- react-frontend/src/components/Mapping.tsx
+++ react-frontend/src/components/Mapping.tsx
@@ -1,14 +1,16 @@
-import { Grid, MenuItem, Select, Button } from "@material-ui/core";
+import { Grid, MenuItem, Select, Button, makeStyles } from "@material-ui/core";
 import { Loader } from "google-maps";
 import { FormEvent, useCallback, useEffect, useRef, useState } from "react";
 import { makeCarIcon, makeMarkerIcon, Map } from "../util/map";
 import { Route } from "../util/models";
 import { sample, shuffle } from "lodash";
-import { getCurrentPosition } from './../util/geolocation';
-import { RouteExistsError } from './../errors/route-exists.error';
+import { getCurrentPosition } from "./../util/geolocation";
+import { RouteExistsError } from "./../errors/route-exists.error";
 import { useSnackbar } from "notistack";
+import { Navbar } from "./Navbar";
+import io from "socket.io-client";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.REACT_APP_API_URL as string;
 
 const googleMapsLoader = new Loader(process.env.REACT_APP_GOOGLE_API_KEY);
 
@@ -25,12 +27,64 @@ const colors = [
   "#827717",
 ];
 
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+    height: "100%",
+  },
+  form: {
+    margin: "16px",
+  },
+  btnSubmitWrapper: {
+    textAlign: "center",
+    marginTop: "8px",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
 type Props = {};
 export const Mapping = (props: Props) => {
+  const classes = useStyles();
   const [routes, setRoutes] = useState<Route[]>([]);
   const [routeIdSelected, setRouteIdSelected] = useState<string>("");
   const mapRef = useRef<Map>();
+  const socketIORef = useRef<SocketIOClient.Socket>();
   const { enqueueSnackbar } = useSnackbar();
+
+  const finishRoute = useCallback((route: Route) => {
+    enqueueSnackbar(`${route.title} finalizou!`, {
+      variant: "success",
+    });
+    mapRef.current?.removeRoute(route._id);
+  }, [enqueueSnackbar]);
+
+  useEffect(() => {
+    if (!socketIORef.current?.connected) {
+      socketIORef.current = io.connect(API_URL);
+      socketIORef.current.on("connect", () => console.log("conectou"));
+    }
+    const handler = (data: {
+      routeId: string;
+      position: [number, number];
+      finished: boolean;
+    }) => {
+      mapRef.current?.moveCurrentMarker(data.routeId, {
+        lat: data.position[0],
+        lng: data.position[1],
+      });
+      const route = routes.find((route) => route._id === data.routeId) as Route;
+      if(data.finished) {
+        finishRoute(route);
+      }
+    };
+    socketIORef.current?.on("new-position", handler);
+    return () => {
+      socketIORef.current?.off("new-position", handler);
+    };
+  }, [finishRoute, routeIdSelected, routes]);
 
   useEffect(() => {
     fetch(`${API_URL}/routes`)
@@ -52,41 +106,46 @@ export const Mapping = (props: Props) => {
     })();
   }, []);
 
-  const startRoute = useCallback((event: FormEvent) => {
-    event.preventDefault();
-    const route = routes.find(route => route._id === routeIdSelected);
-    const color = sample(shuffle(colors)) as string;
-    try {
-      mapRef.current?.addRoute(routeIdSelected, {
-        currentMarkerOptions: {
-          position: route?.startPosition,
-          icon: makeCarIcon(color),
-        },
-        endMarkerOptions: {
-          position: route?.endPosition,
-          icon: makeMarkerIcon(color),
-        },
-      });
-    } catch (error) {
-      if (error instanceof RouteExistsError) {
-        enqueueSnackbar(`${route?.title} já adicionado, esperar finalizar.`, {
-          variant: "error",
+  const startRoute = useCallback(
+    (event: FormEvent) => {
+      event.preventDefault();
+      const route = routes.find((route) => route._id === routeIdSelected);
+      const color = sample(shuffle(colors)) as string;
+      try {
+        mapRef.current?.addRoute(routeIdSelected, {
+          currentMarkerOptions: {
+            position: route?.startPosition,
+            icon: makeCarIcon(color),
+          },
+          endMarkerOptions: {
+            position: route?.endPosition,
+            icon: makeMarkerIcon(color),
+          },
         });
-        return;
+        socketIORef.current?.emit("new-direction", {
+          routeId: routeIdSelected,
+        });
+      } catch (error) {
+        if (error instanceof RouteExistsError) {
+          enqueueSnackbar(`${route?.title} já adicionado, esperar finalizar.`, {
+            variant: "error",
+          });
+          return;
+        }
+        throw error;
       }
-      throw error;
-    }
-  },
-   [routeIdSelected, routes]
+    },
+    [routeIdSelected, routes, enqueueSnackbar]
   );
 
   return (
-    <Grid container style={{width: '100%', height: '100%'}}>
+    <Grid className={classes.root} container>
       <Grid item xs={12} sm={3}>
-        <form onSubmit={startRoute}>
-          <Select 
+        <Navbar />
+        <form onSubmit={startRoute} className={classes.form}>
+          <Select
             fullWidth
-            value={routeIdSelected} 
+            value={routeIdSelected}
             onChange={(event) => setRouteIdSelected(event.target.value + "")}
           >
             <MenuItem value="">
@@ -98,13 +157,15 @@ export const Mapping = (props: Props) => {
               </MenuItem>
             ))}
           </Select>
-          <Button type="submit" color="primary" variant="contained">
-            Iniciar uma corrida
-          </Button>
+          <div className={classes.btnSubmitWrapper}>
+            <Button type="submit" color="primary" variant="contained">
+              Iniciar uma corrida
+            </Button>
+          </div>
         </form>
       </Grid>
       <Grid item xs={12} sm={9}>
-        <div id="map" style={{width: '100%', height: '100%'}}></div>
+        <div id="map" className={classes.map} />
       </Grid>
     </Grid>
   );
